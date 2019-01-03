d263f3a794f969dcf890d57ef2a90e90003e42a4
Nikita Mikhnenko <abernod137@gmail.com>
2019-01-03 21:28:02 +0300

    adding user feed

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from bookmarks.common.decorators import ajax_required
+from actions.utils import create_action
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -22,6 +23,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new item detail view
@@ -54,6 +56,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
